86fbab96aa42ad75605b975578da66e5d0e10b3b
Chini Sinha <chinisinha@gmail.com>
2017-01-16 05:32:58 -0500

    Chini: Fixing Heroku MLab link

--- test/app.js
+++ test/app.js
@@ -4,14 +4,10 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://heroku_5xq59zcw:jq9trku1dhtbfuhq8acqcp1866@ds111529.mlab.com:11529/heroku_5xq59zcw';
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+    if(process.env.MONGODB_URI) {
+        connectionString = process.env.MONGODB_URI;
     }
 
     var mongoose = require("mongoose");
